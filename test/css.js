3f94bc2b862b3eefd45475080a99f3398e35170b
Pascal Duez <public@pascalduez.me>
2023-02-07 10:33:06 +0100

    Switch to Node test runner

--- test/css.js
+++ test/css.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const test = require('ava');
+const path = require('node:path');
+const test = require('node:test');
+const assert = require('node:assert/strict');
 const stylelint = require('stylelint');
 
 const config = {
   extends: ['stylelint-config-standard', path.join(__dirname, '..')],
 };
 
-test('should not results errors nor warnings', async t => {
+test('should not results errors nor warnings', async () => {
   const data = await stylelint.lint({
     config,
     files: 'test/*.css',
@@ -15,6 +16,6 @@ test('should not results errors nor warnings', async t => {
   const { errored, results } = data;
   const { warnings } = results[0];
 
-  t.falsy(errored, 'no errors');
-  t.is(warnings.length, 0, 'no warnings');
+  assert.equal(errored, false, 'no errors');
+  assert.equal(warnings.length, 0, 'no warnings');
 });
